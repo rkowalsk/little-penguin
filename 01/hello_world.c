17e24337868cbecdba2013bc2397e8da323614f0
rkowalsk <rkowalsk@student.42lyon.fr>
2023-05-23 16:31:57 +0200

    finished 01

--- 01/hello_world.c
+++ 01/hello_world.c
@@ -2,18 +2,19 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
-//MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Romain Kowalski");
-MOUDLE_DESCRIPTION("A sussy module");
+MODULE_DESCRIPTION("A simple module");
 
 static int __init hello_world_init(void)
 {
-	printk(KERN_WARNING "SUS");
+	printk(KERN_INFO"Hello World\n");
+	return (0);
 }
 
 static void __exit hello_world_cleanup(void)
 {
-	printk(KERN_WARNING "AMOGUS");
+	printk(KERN_INFO"J'me tire\n");
 }
 
 module_init(hello_world_init);
