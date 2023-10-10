cbb6b8d19ad38079b4c39478397e997a6ad2a75f
Romain Kowalski <romain.kowalski@proton.me>
2023-10-10 17:32:16 +0200

    fix: 08 not working and not doing the right thing

--- 08/weird_ass_module.c
+++ 08/weird_ass_module.c
@@ -3,6 +3,7 @@
 #include <linux/init.h>
 #include <linux/fs.h>
 #include <linux/slab.h>
+#include <linux/miscdevice.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
@@ -27,30 +28,50 @@ static struct miscdevice myfd_device = {
 };
 
 static char	buffer[PAGE_SIZE];
+static char	tmp[PAGE_SIZE];
 
 static int __init myfd_init (void)
 {
-	return (misc_register(&myfd_device));
+	unsigned int	ret = misc_register(&myfd_device);
+	if (ret < 0) {
+		pr_err("Registrering module %s failed: %d\n", myfd_device.name, ret);
+		return (ret);
+	}
+	buffer[0] = 0;
+	pr_info("Module %s loaded\n", myfd_device.name);
+	return (0);
 }
 
 static void __exit myfd_cleanup (void)
 {
+	misc_deregister(&myfd_device);
+	pr_info("Module %s unloaded\n", myfd_device.name);
 }
 
 ssize_t	myfd_read(struct file *fp, char __user *user, size_t size,
 					loff_t *offs)
 {
-	size_t	buff_size = strlen(buffer);
-	return simple_read_from_buffer(user, size, offs, buffer, buff_size);
+	ssize_t	i;
+	ssize_t	j;
+
+	j = strlen(buffer) - 1;
+	i = 0;
+	while (j >= 0) {
+		tmp[i] = buffer[j];
+		i++;
+		j--;
+	}
+	tmp[i] = 0;
+	return simple_read_from_buffer(user, size, offs, tmp, i);
 }
 
 static ssize_t myfd_write(struct file *fp, const char __user *user, size_t size,
 					loff_t *offs)
 {
 	ssize_t	res;
-	res = simple_write_to_buffer(buffer, PAGE_SIZE, offs, user, size) + 1;
-	if (res > 0)
-		buffer[res + 1] = '\0';
+	res = simple_write_to_buffer(buffer, PAGE_SIZE - 1, offs, user, size);
+	if (res >= 0)
+		buffer[res] = '\0';
 	return res;
 }
 
