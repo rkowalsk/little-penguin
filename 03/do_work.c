5e7bcc516e0cc9760c8e209921744e02e1a47ea5
Romain Kowalski <romain.kowalski@proton.me>
2024-02-21 16:38:17 +0100

    fixed norm

--- 03/do_work.c
+++ 03/do_work.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-only
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/delay.h>
@@ -8,6 +9,7 @@ int do_work(int *my_int, int retval)
 	int x;
 	int y = *my_int;
 	int z;
+
 	for (x = 0; x < my_int; ++x)
 		udelay(10);
 	if (y < 10) {
@@ -22,6 +24,7 @@ int do_work(int *my_int, int retval)
 int my_init(void)
 {
 	int x = 10;
+
 	x = do_work(&x, x);
 	return x;
 }
--- 08/weird_ass_module.c
+++ 08/weird_ass_module.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-only
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -10,12 +11,12 @@ MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
 MODULE_DESCRIPTION("Creates a file to write to and read in reverse from.");
 
 static ssize_t	myfd_read(struct file *fp, char __user *user, size_t size,
-					loff_t *offs);
+			  loff_t *offs);
 
 static ssize_t	myfd_write(struct file *fp, const char __user *user,
-					size_t size, loff_t *offs);
+			   size_t size, loff_t *offs);
 
-static struct file_operations fops = {
+static const struct file_operations fops = {
 	.owner = THIS_MODULE,
 	.read = &myfd_read,
 	.write = &myfd_write
@@ -30,26 +31,27 @@ static struct miscdevice myfd_device = {
 static char	buffer[PAGE_SIZE];
 static char	tmp[PAGE_SIZE];
 
-static int __init myfd_init (void)
+static int __init myfd_init(void)
 {
 	unsigned int	ret = misc_register(&myfd_device);
+
 	if (ret < 0) {
 		pr_err("Registrering module %s failed: %d\n", myfd_device.name, ret);
-		return (ret);
+		return ret;
 	}
 	buffer[0] = 0;
 	pr_info("Module %s loaded\n", myfd_device.name);
-	return (0);
+	return 0;
 }
 
-static void __exit myfd_cleanup (void)
+static void __exit myfd_cleanup(void)
 {
 	misc_deregister(&myfd_device);
 	pr_info("Module %s unloaded\n", myfd_device.name);
 }
 
 ssize_t	myfd_read(struct file *fp, char __user *user, size_t size,
-					loff_t *offs)
+		  loff_t *offs)
 {
 	ssize_t	i;
 	ssize_t	j;
@@ -66,9 +68,10 @@ ssize_t	myfd_read(struct file *fp, char __user *user, size_t size,
 }
 
 static ssize_t myfd_write(struct file *fp, const char __user *user, size_t size,
-					loff_t *offs)
+			  loff_t *offs)
 {
 	ssize_t	res;
+
 	res = simple_write_to_buffer(buffer, PAGE_SIZE - 1, offs, user, size);
 	if (res >= 0)
 		buffer[res] = '\0';
